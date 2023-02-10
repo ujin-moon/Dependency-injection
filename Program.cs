2d902f7f9b669cc105324d813ab65672c7d4ffb0
ujin-moon <evgeniy.sedin@internet.ru>
2023-02-11 00:08:57 +0300

    Реализация dependency injection с созданием интерфейса и класса

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public interface ISound
+    {
+        void dosomethink(string message);
+    }
+    public class Sound : ISound
+    {
+        public void dosomethink(string message)
+        {
+            Console.WriteLine($"Собака сказала: {message}");
+        }
+    }
+    public class Dog
+    {
+        private readonly ISound _sound;
+
+        public Dog(ISound sound)
+        {
+            _sound = sound;
+        }
+        public void dosomethink(string message)
+        {
+            _sound.dosomethink(message);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dog = new Dog(new Sound());
+            dog.dosomethink("ГАВ-ГАВ");
+            Console.ReadLine();
+        }
+    }
+}
